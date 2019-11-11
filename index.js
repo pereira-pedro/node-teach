9295c3f05353145d5c121120fe2f6258af0d911a
Pedro Bastos <pedro@pontonet.com>
2019-11-11 16:26:36 -0400

    week09

--- index.js
+++ index.js
@@ -1,10 +1,33 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+const express = require("express");
+const path = require("path");
+const PORT = process.env.PORT || 5000;
 
 express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+  .use(express.static(path.join(__dirname, "public")))
+  .set("views", path.join(__dirname, "views"))
+  .set("view engine", "ejs")
+  .get("/", (req, res) => res.render("pages/index"))
+  .get("/math_service", (req, res) => {
+    var result;
+
+    eval(`result = ${req.query.ope1}${req.query.operator}${req.query.ope2}`);
+
+    res.json({
+      result: result
+    });
+    res.end();
+  })
+  .get("/math", (req, res) => {
+    var result;
+
+    eval(`result = ${req.query.ope1}${req.query.operator}${req.query.ope2}`);
+
+    res.render("math", {
+      operand1: req.query.ope1,
+      operator: req.query.operator,
+      operand2: req.query.ope2,
+      result: result
+    });
+    res.end();
+  })
+  .listen(PORT, () => console.log(`Listening on ${PORT}`));
